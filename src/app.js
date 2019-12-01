63020e2cc8c21fee54ec34afed34854b9f337578
Wendy Lee <wendy.lee@thoughtworks.com>
2019-12-01 23:42:09 +0000

    [PRMT-748] Add swagger documentation and rename endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import upload from "./services/upload";
+import httpContext from 'express-http-context';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use(httpContext.middleware);
 
-app.post('/ehr', (req, res) => {
+app.get('/health', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.post('/url', (req, res) => {
     if(Object.keys(req.body).length ===0){
         res.sendStatus((400));
     }
