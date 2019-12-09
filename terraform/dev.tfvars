76b88586446dca10589e923c8e40f6e331013158
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-12-09 17:42:36 +0000

    [PRMT-760] Setup ALB target in ECS service

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -11,8 +11,8 @@ task_container_name  = "ehr-repo-container"
 task_image_name      = "deductions/ehr-repo"
 task_cpu             = 256
 task_memory          = 512
-task_container_port  = 80
-task_host_port       = 80
+task_container_port  = 3000
+task_host_port       = 3000
 
 service_container_port  = "3000"
 service_container_name  = "ehr-repo-container"
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,9 +1,9 @@
 locals {
   deductions_private_alb_dns = data.terraform_remote_state.prm-deductions-infra.outputs.deductions_core_alb_dns
 }
-resource "aws_route53_record" "gp2gp-r53-record" {
+resource "aws_route53_record" "ehr-r53-record" {
   zone_id = data.aws_ssm_parameter.root_zone_id.value
-  name    = "dev.ehr-repo"
+  name    = "${var.environment}.ehr-repo"
   type    = "CNAME"
   ttl     = "300"
   records = [local.deductions_private_alb_dns]
