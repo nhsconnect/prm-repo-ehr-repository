4b6eac59bfc358e25f4f57c413cedbcfc1ae0618
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-12-11 10:52:55 +0000

    [PRMT-760] Fix s3 bucket config variable

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    awsS3BucketName: 'deductor-dev',
+    awsS3BucketName: process.env.S3_BUCKET_NAME,
     databaseUser: process.env.DATABASE_USER,
     databasePassword: process.env.DATABASE_PASSWORD,
     databaseName: process.env.DATABASE_NAME,
