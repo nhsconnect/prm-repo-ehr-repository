b4190c3c49650dd894331bec78a9adab9a607247
Amy Welch <awelch@thoughtworks.com>
2019-12-13 18:32:32 +0000

    [PRMT-792] fix integration test with incorrect request body

--- infra-tests/test_upload.js
+++ infra-tests/test_upload.js
@@ -1,28 +1,26 @@
 const axios = require('axios');
 
-axios.post(process.env.EHR_URL + '/url',{
-    "ConversationID": "1234",
-    "RegistrationID": "4321"
-})
-.then((response) => {
-  console.log(response);
-  if(response.status != 202) {
-    process.exit(5);
-  }
-  var options = {};
-  url = response.data
-  axios.put(url, 'hello', options)
-    .then(res=> {
-      console.log(res);
-      if(res.status != 200) {
-        process.exit(6);
-      }
-    })
-    .catch(err=> {
-      console.log(err);
-      process.exit(7);
-    });
-}, (error) => {
-  console.log(error);
-  process.exit(8);
-});
+axios.post(process.env.EHR_URL + '/url', { 'conversationId': '1234' })
+  .then(response => {
+    console.log(response);
+    if (response.status !== 202) {
+      process.exit(5);
+    }
+    var options = {};
+    url = response.data;
+    axios.put(url, 'hello', options)
+      .then(res => {
+        console.log(res);
+        if (res.status !== 200) {
+          process.exit(6);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        process.exit(7);
+      });
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(8);
+  });
