d3aa00d91c10982372e7e8a5c38a1e6072edde29
bfisher <fishey2b@gmail.com>
2020-01-09 10:02:13 +0000

    [PRMT-541] Config isLocal added

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,7 @@ const config = {
   awsS3BucketName: process.env.S3_BUCKET_NAME,
   databaseUser: process.env.DATABASE_USER,
   databasePassword: process.env.DATABASE_PASSWORD,
+  isLocal: process.env.NODE_ENV === 'local',
   databaseName: process.env.DATABASE_NAME,
   databaseHost: process.env.DATABASE_HOST,
   sequelize: sequelizeConfig[process.env.NODE_ENV || 'local']
