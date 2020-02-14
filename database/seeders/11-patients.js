ac7cdb14f838c3ae76819e8569bbe4c562be19f1
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2020-02-14 17:22:49 +0000

    [PRMT-959] Fixed test by updating patient nhs_number

--- database/seeders/11-patients.js
+++ database/seeders/11-patients.js
@@ -7,7 +7,7 @@ module.exports = {
     return queryInterface.bulkInsert(tableName, [
       {
         id: '3779fb0c-4142-43a4-a9b8-c5f5f3f632cc',
-        nhs_number: 4444444444,
+        nhs_number: 8888888888,
         created_at: new Date(),
         updated_at: new Date()
       }
