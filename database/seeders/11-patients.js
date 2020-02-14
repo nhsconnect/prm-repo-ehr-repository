44420c9dc1d12839de7bb5bb0c01d13778bea9e9
Helen Zhou <helen.zhou@thoughtworks.com>
2020-02-14 15:57:29 +0000

    [PRMT-959] - Add patient, health-record and message-fragment dummy data for database seeding

---
+++ database/seeders/11-patients.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const tableName = 'patients';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(tableName, [
+      {
+        id: '3779fb0c-4142-43a4-a9b8-c5f5f3f632cc',
+        nhs_number: 4444444444,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(tableName, null, {});
+  }
+};
---
+++ database/seeders/32-message-fragment-table.js
@@ -0,0 +1,145 @@
+'use strict';
+
+const tableName = 'message_fragments';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(tableName, [
+      {
+        id: 'd1738626-0cb2-402f-b56e-89bb1e65cf1e',
+        message_id: 'dc0626d8-bb01-4b94-a044-9b9322b52634',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: '04523969-6679-4ac8-8222-c226ff7a155f'
+      },
+      {
+        id: 'c7fb1c74-9685-4000-ba29-babd4a5cfa97',
+        message_id: '37d13525-2877-4af9-aeac-985e7a5a6ab1',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        health_record_id: '7a1515b9-4d2d-415c-b4e2-09392af8fec4'
+      },
+      {
+        id: '11268d6f-e017-4397-b875-45e46bfded20',
+        message_id: '8c477c1d-e8d9-4471-b6ac-4be84c20a7b9',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: 'a717d32e-fe34-42c5-a564-9d6b3049b624'
+      },
+      {
+        id: '27c68edf-b624-4013-8c5e-a5ba6b66d0bf',
+        message_id: '1395d65b-f1a4-42de-9392-3741909d3cee',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: 'a717d32e-fe34-42c5-a564-9d6b3049b624'
+      },
+      {
+        id: '257c6dc7-8fe1-4c14-83c2-0736bc4e0619',
+        message_id: '372c0366-00c4-425e-8509-a0aed5aad420',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        health_record_id: 'a717d32e-fe34-42c5-a564-9d6b3049b624'
+      },
+      {
+        id: '0076db22-310b-4dfb-aa2f-46f62cfa5efe',
+        message_id: '4dd7d349-e9a3-4cf1-b7b5-08cb18b590b0',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: 'deeb8288-df5f-4197-a5b9-743992f4d10f'
+      },
+      {
+        id: 'b2ae3cfc-4a6a-47f0-842a-11d17ad75c05',
+        message_id: '64949ae0-e742-4807-ab74-304312746dbf',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: 'deeb8288-df5f-4197-a5b9-743992f4d10f'
+      },
+      {
+        id: 'ecb31bb6-a883-4f5b-a89d-ca291209abca',
+        message_id: '6a66d22e-9d13-491c-b26e-3db31296831d',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: 'deeb8288-df5f-4197-a5b9-743992f4d10f'
+      },
+      {
+        id: '06782d34-0a07-4cf0-9ae9-984c1e92c276',
+        message_id: '7abd0470-2ff5-44db-ae96-843ca680344c',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: '5dc9d99c-c018-46d3-bba2-49bc4489e529'
+      },
+      {
+        id: '63cfbc2c-d949-4c49-90a7-dfea823c2c3a',
+        message_id: 'f6b5e9df-bdba-45a3-a3ed-58ee16b5b0d6',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: '5dc9d99c-c018-46d3-bba2-49bc4489e529'
+      },
+      {
+        id: 'cafbe945-cd48-4a9e-8810-81898b75e839',
+        message_id: '6cb02d19-c197-4178-a697-bb058f221ee5',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        health_record_id: '5dc9d99c-c018-46d3-bba2-49bc4489e529'
+      },
+      {
+        id: '28d3e466-b5b5-463c-b632-f1ae57ef209b',
+        message_id: '39a4c465-ba0c-4798-ba09-362a315d01fe',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        health_record_id: '08850511-50c8-436e-b12c-5c452758c4ec'
+      },
+      {
+        id: 'd06f1363-e8d3-4092-bc62-23b46b0722f5',
+        message_id: '171826c5-685a-433f-a530-b266ca4f7d16',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        health_record_id: '08850511-50c8-436e-b12c-5c452758c4ec'
+      },
+      {
+        id: '777d1123-6314-4f24-bc0c-b0310edb4f47',
+        message_id: 'e3c5493c-f758-45b7-8bb2-408c0de31f75',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: '4aa3f0fa-197d-4af3-b64f-4a125cbefeb0'
+      },
+      {
+        id: '5bfb1af4-ec8c-4a39-bb80-a154493d3faa',
+        message_id: '02268409-1666-403a-9722-18cfee8aa520',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: '4aa3f0fa-197d-4af3-b64f-4a125cbefeb0'
+      },
+      {
+        id: '9637758b-cb1e-482e-b4f3-74701a6bc283',
+        message_id: 'ddac4a5e-cb86-4077-bf10-2292feef08c5',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: '4aa3f0fa-197d-4af3-b64f-4a125cbefeb0'
+      },
+      {
+        id: '675d6744-63b0-43ab-ad5e-04fa22dd94f8',
+        message_id: 'b7117e20-8408-49b5-aaaf-d42d428183f4',
+        created_at: 'new Date()',
+        updated_at: 'new Date()',
+        completed_at: 'new Date()',
+        health_record_id: '4aa3f0fa-197d-4af3-b64f-4a125cbefeb0'
+      }
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(tableName, null, {});
+  }
+};
