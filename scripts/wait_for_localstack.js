bfe75374f8df1a7e52fec1239699da127b2c1452
bfisher <fishey2b@gmail.com>
2020-03-13 15:17:58 +0000

    [PRMT-1041] Added a script that will wait for localstack to load

---
+++ scripts/wait_for_localstack.js
@@ -0,0 +1,50 @@
+const { Endpoint, S3 } = require('aws-sdk');
+
+const MAX_ATTEMPTS = 30;
+
+
+const isConnected = () => {
+  return new Promise((resolve, reject) => {
+    const s3 = new S3({
+      accessKeyId: 'test-access-key',
+      secretAccessKey: 'test-secret-key',
+      endpoint: new Endpoint(process.env.LOCALSTACK_URL),
+      s3ForcePathStyle: true
+    });
+
+    s3.headBucket(
+      {
+        Bucket: process.env.S3_BUCKET_NAME
+      },
+      err => {
+        if (err) resolve(false);
+        resolve(true);
+      }
+    );
+  });
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+const checkIfConnected = async () => {
+  for(let count = 0; count <= MAX_ATTEMPTS;  ++count) {
+
+    console.log(`Attempting to connect to bucket in localstack (Attempt ${count} of ${MAX_ATTEMPTS})`);
+
+    const result = await isConnected();
+
+    if(result) {
+      console.log('Successfully connected to localstack');
+      return;
+    }
+    await sleep(1000);
+  }
+
+  throw new Error(`Connection to Localstack timedout after ${MAX_ATTEMPTS}`)
+};
+
+checkIfConnected();
