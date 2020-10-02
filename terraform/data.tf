d04703178b39b6baad0993dbc25afc345bd9695c
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-10-02 10:10:38 +0100

    [PRMT-1121] Fix typo in ssm key name

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_ssm_parameter" "deductions_core_vpc_id" {
-  name = "/repo/${var.environment}/output/prm-deductions/deductions-core-vpc-id"
+  name = "/repo/${var.environment}/output/prm-deductions-infra/deductions-core-vpc-id"
 }
 
 data "aws_ssm_parameter" "root_zone_id" {
