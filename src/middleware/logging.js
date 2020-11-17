f00ac2d05db50a075f30f7b6765f5755b58e4b26
Rosie Amphlett <rosie.amphlett1@nhs.net>
2020-11-17 13:53:09 +0000

    [PRMT-1004] Updated logging so we can see ehr repo logs

--- src/middleware/logging.js
+++ src/middleware/logging.js
@@ -4,8 +4,10 @@ import logger from '../config/logging';
 
 const LOG_EVENT_KEY = 'logEvent';
 
-export const updateLogEvent = event =>
+export const updateLogEvent = event => {
   httpContext.set(LOG_EVENT_KEY, merge(httpContext.get(LOG_EVENT_KEY), event));
+  logger.info('New event info', event);
+};
 
 export const updateLogEventWithError = err =>
   updateLogEvent({ error: { ...err, message: err.message, stack: err.stack } });
