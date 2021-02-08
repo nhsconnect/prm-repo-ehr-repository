66aae57153ac79c2b1b924f548cc8b8f77018e41
Tomasz Setkowski <tomasz.setkowski2@nhs.net>
2021-02-08 17:20:05 +0000

    PRMT-1554 cleanup script and sanitation of s3 paths

--- src/services/storage/get-signed-url.js
+++ src/services/storage/get-signed-url.js
@@ -3,7 +3,8 @@ import { logEvent } from '../../middleware/logging';
 
 const getSignedUrl = (conversationId, messageId, operation) => {
   logEvent('Retrieving pre-signed URL');
-
+  conversationId = conversationId.toLowerCase();
+  messageId = messageId.toLowerCase();
   const s3Service = new S3Service(`${conversationId}/${messageId}`);
   return s3Service.getPresignedUrl(operation);
 };
