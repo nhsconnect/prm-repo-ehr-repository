7ac0a89c5b1a54cca5a7863b532132239d78ad07
aiwatko <oktawia.kata1@nhs.net>
2021-02-09 14:04:53 +0000

    [PRMT-1554] Tested id transformation to lowercase in get-presigned-url

--- src/services/storage/__tests__/get-signed-url.test.js
+++ src/services/storage/__tests__/get-signed-url.test.js
@@ -4,9 +4,9 @@ import getSignedUrl from '../get-signed-url';
 jest.mock('aws-sdk');
 
 describe('getSignedUrl', () => {
-  const conversationId = 'some-id';
-  const messageId = 'some-message';
   ['getObject', 'putObject'].forEach(operation => {
+    const conversationId = 'some-id';
+    const messageId = 'some-message';
     it('should return presigned url from s3 for storing ehr', async () => {
       const mockGetSignedUrlPromise = jest.fn().mockResolvedValue('some-url');
       S3.mockImplementation(() => ({
@@ -17,5 +17,21 @@ describe('getSignedUrl', () => {
       expect(mockGetSignedUrlPromise).toHaveBeenCalledWith(operation, expect.anything());
       expect(url).toEqual('some-url');
     });
+  });
+
+  it('should normalise IDs to lowercase', async () => {
+    const conversationId = 'SOME-ID';
+    const messageId = 'SOME-MESSAGE';
+
+    const mockGetSignedUrlPromise = jest.fn();
+    S3.mockImplementation(() => ({
+      getSignedUrlPromise: mockGetSignedUrlPromise
+    }));
+
+    await getSignedUrl(conversationId, messageId, 'getObject');
+    expect(mockGetSignedUrlPromise).toHaveBeenCalledWith(
+      'getObject',
+      expect.objectContaining({ Key: `${conversationId.toLowerCase()}/${messageId.toLowerCase()}` })
+    );
   });
 });
