d9a3c44ca5f936797cda92b8ed897527060786b1
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-02-26 17:20:30 +0000

    [PRMT-1228] Added performance test for an EHR with a large attachment that has 50 parts

--- test/performance/ehr-with-100s-small-attachments.test.js
+++ test/performance/ehr-with-100s-small-attachments.test.js
@@ -2,13 +2,113 @@ import { v4 } from 'uuid';
 import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
 
-describe('Performance of EHR with hundreds of small attachments', () => {
-  it('should save everything within 10 seconds', async () => {
-    // Send ehr with 100 attachments to ehr repo -
+const headers = { Authorization: process.env.AUTHORIZATION_KEYS };
+
+async function sendAttachment(id, conversationId, attachmentMessageIds) {
+  const attachment = {
+    data: {
+      type: 'messages',
+      id,
+      attributes: {
+        conversationId,
+        messageType: 'attachment',
+        attachmentMessageIds
+      }
+    }
+  };
+
+  const response = await axios.post(`${process.env.SERVICE_URL}/messages`, attachment, {
+    adapter,
+    headers
+  });
+  // Assert 201 for each attachment
+  expect(response.status).toEqual(201);
+}
+
+async function sendEhrExtract(
+  ehrExtractMessageId,
+  conversationId,
+  nhsNumber,
+  ehrExtractAttachments
+) {
+  const ehrExtract = {
+    data: {
+      type: 'messages',
+      id: ehrExtractMessageId,
+      attributes: {
+        conversationId,
+        nhsNumber,
+        messageType: 'ehrExtract',
+        attachmentMessageIds: ehrExtractAttachments
+      }
+    }
+  };
+
+  const ehrResponse = await axios.post(`${process.env.SERVICE_URL}/messages`, ehrExtract, {
+    adapter,
+    headers
+  });
+
+  expect(ehrResponse.status).toEqual(201);
+}
+
+const timeAction = async (description, action) => {
+  const before = new Date();
+  await action();
+  const after = new Date();
+
+  // Assert it returns 201
+  // Measure how long it took for ehr extract to be stored
+  console.log(description, after - before, 'ms');
+};
+
+const testPerformance = async (
+  smallAttachmentMessageIds,
+  largeAttachmentId,
+  partsOfLargeAttachment
+) => {
+  const ehrExtractMessageId = v4();
+  const conversationId = v4();
+  const nhsNumber = '1234567890';
+
+  let ehrExtractAttachments = smallAttachmentMessageIds.slice();
+  if (largeAttachmentId) {
+    ehrExtractAttachments = smallAttachmentMessageIds.concat([largeAttachmentId]);
+  }
+
+  await timeAction('Time taken to store health record extract: ', () =>
+    sendEhrExtract(ehrExtractMessageId, conversationId, nhsNumber, ehrExtractAttachments)
+  );
+
+  await timeAction('Time taken to store all attachments: ', async () => {
+    if (largeAttachmentId) {
+      await sendAttachment(largeAttachmentId, conversationId, partsOfLargeAttachment);
+    }
+
+    const allSmallAttchments = smallAttachmentMessageIds.concat(partsOfLargeAttachment);
+
+    for (const id of allSmallAttchments) {
+      await sendAttachment(id, conversationId, []);
+    }
+  });
+
+  // Retrieve ehr
+  const retrieval = await axios.get(
+    `${process.env.SERVICE_URL}/new/patients/${nhsNumber}/health-records/${conversationId}`,
+    {
+      headers,
+      adapter
+    }
+  );
+
+  // Assert 200 and assert correct number of attachments
+  expect(retrieval.status).toEqual(200);
+};
+
+describe('Performance of EHR', () => {
+  it('Performance of EHR with hundreds of small attachments - within 10 seconds', async () => {
+    //send ehr with 100 attachments to ehr repo -
     // POST /messages with type ehrExtract and 100 entries in the attachment list
-    const ehrExtractMessageId = v4();
-    const conversationId = v4();
-    const nhsNumber = '1234567890';
     let attachmentMessageIds = [];
 
     const noOfAttachments = 100;
@@ -16,77 +116,27 @@ describe('Performance of EHR with hundreds of small attachments', () => {
       attachmentMessageIds.push(v4());
     }
 
-    const ehrExtract = {
-      data: {
-        type: 'messages',
-        id: ehrExtractMessageId,
-        attributes: {
-          conversationId,
-          nhsNumber,
-          messageType: 'ehrExtract',
-          attachmentMessageIds
-        }
-      }
-    };
+    await testPerformance(attachmentMessageIds, null, []);
+  }, 10000);
 
-    const headers = { Authorization: process.env.AUTHORIZATION_KEYS };
+  it('Performance of EHR with 50 attachments, one of which is large - within 25 seconds', async () => {
+    // Send ehr with 50 attachments to store in EHR repo database
+    let attachmentMessageIds = [];
 
-    const beforeEhrExtract = new Date();
-    const response = await axios.post(`${process.env.SERVICE_URL}/messages`, ehrExtract, {
-      adapter,
-      headers
-    });
-    const afterEhrExtract = new Date();
-
-    //assert it returns 201
-    //measure how long it took
-    console.log(
-      'Time taken to store health record extract: ',
-      afterEhrExtract - beforeEhrExtract,
-      'ms'
-    );
-    expect(response.status).toEqual(201);
-
-    //send each attachment to ehr repo - POST /messages with type attachment for each
-    const beforeAttachments = new Date();
-    for (const id of attachmentMessageIds) {
-      const attachment = {
-        data: {
-          type: 'messages',
-          id,
-          attributes: {
-            conversationId,
-            messageType: 'attachment',
-            attachmentMessageIds: []
-          }
-        }
-      };
-      const response = await axios.post(`${process.env.SERVICE_URL}/messages`, attachment, {
-        adapter,
-        headers
-      });
-      //assert 201 for each attachment
-      expect(response.status).toEqual(201);
+    const noOfAttachments = 50;
+    for (let i = 0; i < noOfAttachments; i++) {
+      attachmentMessageIds.push(v4());
     }
 
-    //measure how long it took
-    const afterAttachments = new Date();
-    console.log(
-      'Time taken to store all attachments: ',
-      afterAttachments - beforeAttachments,
-      'ms'
-    );
+    // Create one large attachment which is made up of 50 parts
+    let largeAttachments = [];
+    const noOfPartsOfVeryLargeAttachment = 50;
+    const veryLargeAttachmentId = v4();
+    for (let i = 0; i < noOfPartsOfVeryLargeAttachment - 1; i++) {
+      largeAttachments.push(v4());
+    }
 
-    //retrieve ehr
-    const retrieval = await axios.get(`${process.env.SERVICE_URL}/new/patients/${nhsNumber}`, {
-      headers,
-      adapter
-    });
-    //assert 200
-    expect(retrieval.status).toEqual(200);
-    expect(retrieval.data.data.links.attachments.length).toEqual(noOfAttachments);
-
-    //measure how long it took
-    console.log('Time taken to store entire EHR: ', afterAttachments - beforeEhrExtract, 'ms');
-  }, 10000);
+    // Run test
+    await testPerformance(attachmentMessageIds, veryLargeAttachmentId, largeAttachments);
+  }, 25000);
 });
