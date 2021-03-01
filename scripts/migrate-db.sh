496e957be235c578b4a259637b048cf70b952753
ewrobel-nhs <edyta.wrobel@nhs.net>
2021-03-01 16:08:21 +0000

    [PRMT-1529] Logging structure for bash scripts;

--- scripts/migrate-db.sh
+++ scripts/migrate-db.sh
@@ -3,32 +3,41 @@
 # This script executes on docker container start.
 # It runs the DB migration and then starts node.js server
 
+#Structured logging
+NHS_SERVICE=ehr-repository
+timestamp() {
+  date +"%Y-%m-%d %H:%M:%S"
+}
+function jsonPrettify {
+  echo "{message: $1, level: $2, timestamp: `timestamp`, service: ${NHS_SERVICE}, environment: ${NHS_ENVIRONMENT} } "
+}
+
 DB_CONNECTION_TIMEOUT=30
 
 if [ "${EHR_REPO_SKIP_MIGRATION}" == "true" ]; then
-  echo "EHR_REPO_SKIP_MIGRATION is set to true. Skipping DB migration"
+  jsonPrettify "EHR_REPO_SKIP_MIGRATION is set to true. Skipping DB migration" INFO
 else
-  echo "Waiting for DB port to become open 5432"
+  jsonPrettify "Waiting for DB port to become open 5432" INFO
   count=0
   while ! pg_isready -h ${DATABASE_HOST}; do
-    echo "Waiting for ${DATABASE_HOST}:5432"
+    jsonPrettify "Waiting for ${DATABASE_HOST}:5432" INFO
     sleep 1
     ((count++))
     if [ "${DB_CONNECTION_TIMEOUT}" -le $count ]; then
-      echo "Timed-out waiting for DB connection at ${DATABASE_HOST}:5432"
+      jsonPrettify "Timed-out waiting for DB connection at ${DATABASE_HOST}:5432" WARN
       exit 5
     fi
   done
-  echo "DB connection at ${DATABASE_HOST}:5432 is available"
-  echo "Trying to create a database, if not exists. 'Already exists' errors are safe to ignore"
+  jsonPrettify "DB connection at ${DATABASE_HOST}:5432 is available" INFO
+  jsonPrettify "Trying to create a database, if not exists. 'Already exists' errors are safe to ignore" INFO
   PGPASSWORD="${DATABASE_PASSWORD}" createdb --host="${DATABASE_HOST}" \
     --username="${DATABASE_USER}" $DATABASE_NAME || true
   set -e
-  echo "Migrating DB, will not migrate parts that have already been migrated (meta)" && \
+  jsonPrettify "Migrating DB, will not migrate parts that have already been migrated (meta)" INFO && \
   npx sequelize-cli db:migrate
-  echo "DB migration completed."
+  jsonPrettify "DB migration completed." INFO
 fi
 
-echo "Starting node.js server"
+jsonPrettify "Starting node.js server" INFO
 set -e
 exec node build/server.js
