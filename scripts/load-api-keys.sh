6e5b44847683e1b94915e95d881978f5f1ef8d5e
Mircea M <mircea.moise@gmail.com>
2021-05-21 16:59:11 +0100

    [PRMT-1966] Adding / at the end to match the permissions allowed

--- scripts/load-api-keys.sh
+++ scripts/load-api-keys.sh
@@ -2,7 +2,7 @@
 AWS_DEFAULT_REGION=eu-west-2
 
 # Iterates through all api keys in ssm for producer
-for key in $(aws ssm get-parameters-by-path --region ${AWS_DEFAULT_REGION} --path "/repo/${NHS_ENVIRONMENT}/user-input/api-keys/ehr-repo" --recursive | jq -r '.Parameters[].Name')
+for key in $(aws ssm get-parameters-by-path --region ${AWS_DEFAULT_REGION} --path "/repo/${NHS_ENVIRONMENT}/user-input/api-keys/ehr-repo/" --recursive | jq -r '.Parameters[].Name')
 do
   # Gets the value of each api key
   value=$(aws ssm get-parameter --region ${AWS_DEFAULT_REGION} --with-decryption --name "${key}" | jq -r .Parameter.Value)
