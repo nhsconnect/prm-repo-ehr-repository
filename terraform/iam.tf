5510f190bcfe405ea0e48907050879615cdcf9bd
Mircea M <mircea.moise@gmail.com>
2021-05-21 16:46:55 +0100

    [PRMT-1966] Giving permission to the ECS Task to read all ssm parameters that contain API Keys for the consumers of EHR Repo

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -109,7 +109,7 @@ data "aws_iam_policy_document" "ssm_policy_doc" {
 
     resources = [
       "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/user-input/${var.component_name}-authorization-keys",
-      "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/user-input/api-keys/${var.component_name}/e2e-test",
+      "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/user-input/api-keys/${var.component_name}/*",
       "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/output/prm-deductions-infra/core-rds-endpoint",
       "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/user-input/${var.component_name}-db-username",
       "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/user-input/${var.component_name}-db-password"
