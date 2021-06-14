8bb2967cd6eba61dabddbc90ff4b9157eb768cd6
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-06-14 17:53:06 +0100

    [PRMT-2046] Refactored mocking in auth tests

--- src/middleware/__tests__/auth.test.js
+++ src/middleware/__tests__/auth.test.js
@@ -23,105 +23,81 @@ describe('auth', () => {
   const conversationId = uuid();
   const messageId = uuid();
 
-  describe('authenticated successfully', () => {
+  describe('Authenticated successfully', () => {
     it('should return HTTP 200 when correctly authenticated', async () => {
-      initializeConfig.mockReturnValueOnce({ consumerApiKeys: { E2E_TEST: 'correct-key' } });
       const res = await request(app)
         .get(`/messages/${conversationId}/${messageId}`)
         .set('Authorization', 'correct-key');
+
       expect(res.status).toBe(200);
     });
   });
 
   describe('Authorization key environment variables not provided', () => {
-    it('should return 412 if auth keys have not been set', async () => {
+    it('should return 412 with an explicit error message if auth keys have not been set', async () => {
+      const errorMessage = {
+        error: 'Server-side Authorization keys have not been set, cannot authenticate',
+      };
       initializeConfig.mockReturnValueOnce({ consumerApiKeys: {} });
       const res = await request(app)
         .get(`/messages/${conversationId}/${messageId}`)
         .set('Authorization', 'correct-key');
-      expect(res.status).toBe(412);
-    });
 
-    it('should return an explicit error message in the body if authorization keys have not been set', async () => {
-      initializeConfig.mockReturnValueOnce({ consumerApiKeys: {} });
-      const res = await request(app)
-        .get(`/messages/${conversationId}/${messageId}`)
-        .set('Authorization', 'correct-key');
-      expect(res.body).toEqual(
-        expect.objectContaining({
-          error: 'Server-side Authorization keys have not been set, cannot authenticate',
-        })
-      );
+      expect(res.status).toBe(412);
+      expect(res.body).toEqual(errorMessage);
     });
   });
 
   describe('Authorization header not provided', () => {
-    it('should return HTTP 401 when no authorization header provided', async () => {
-      initializeConfig.mockReturnValueOnce({ consumerApiKeys: { TEST_USER: 'correct-key' } });
-      const res = await request(app).get(`/messages/${conversationId}/${messageId}`);
-      expect(res.status).toBe(401);
-    });
-
-    it('should return an explicit error message in the body when no authorization header provided', async () => {
-      initializeConfig.mockReturnValueOnce({ consumerApiKeys: { TEST_USER: 'correct-key' } });
-      const res = await request(app).get(`/messages/${conversationId}/${messageId}`);
-      expect(res.body).toEqual({
+    it('should return HTTP 401 with an explicit error message when no authorization header provided', async () => {
+      const errorMessage = {
         error: 'The request (/messages) requires a valid Authorization header to be set',
-      });
+      };
+      const res = await request(app).get(`/messages/${conversationId}/${messageId}`);
+
+      expect(res.status).toBe(401);
+      expect(res.body).toEqual(errorMessage);
     });
   });
 
-  describe('incorrect Authorisation header value provided ', () => {
-    it('should return HTTP 403 when authorization key is incorrect', async () => {
-      initializeConfig.mockReturnValueOnce({ consumerApiKeys: { TEST_USER: 'correct-key' } });
+  describe('Incorrect Authorisation header value provided ', () => {
+    it('should return HTTP 403 with an explicit error message when authorization key is incorrect', async () => {
+      const errorMessage = { error: 'Authorization header is provided but not valid' };
       const res = await request(app)
         .get(`/messages/${conversationId}/${messageId}`)
         .set('Authorization', 'incorrect-key');
-      expect(res.status).toBe(403);
-    });
 
-    it('should return an explicit error message in the body when authorization key is incorrect', async () => {
-      initializeConfig.mockReturnValueOnce({ consumerApiKeys: { TEST_USER: 'correct-key' } });
-      const res = await request(app)
-        .get(`/messages/${conversationId}/${messageId}`)
-        .set('Authorization', 'incorrect-key');
-      expect(res.body).toEqual(
-        expect.objectContaining({
-          error: 'Authorization header is provided but not valid',
-        })
-      );
+      expect(res.status).toBe(403);
+      expect(res.body).toEqual(errorMessage);
     });
   });
 
   describe('Auth logging', () => {
     it('should log consumer, method and url for correctly authenticated request', async () => {
+      const logMessage = `Consumer: USER_2, Request: GET /messages/${conversationId}/${messageId}`;
       await request(app)
         .get(`/messages/${conversationId}/${messageId}`)
         .set('Authorization', 'key_2');
 
-      expect(logInfo).toHaveBeenCalledWith(
-        `Consumer: USER_2, Request: GET /messages/${conversationId}/${messageId}`
-      );
+      expect(logInfo).toHaveBeenCalledWith(logMessage);
     });
 
     it('should log multiple consumers when they use the same key value', async () => {
+      const logMessage = `Consumer: TEST_USER/DUPLICATE_TEST_USER, Request: GET /messages/${conversationId}/${messageId}`;
       await request(app)
         .get(`/messages/${conversationId}/${messageId}`)
         .set('Authorization', 'correct-key');
 
-      expect(logInfo).toHaveBeenCalledWith(
-        `Consumer: TEST_USER/DUPLICATE_TEST_USER, Request: GET /messages/${conversationId}/${messageId}`
-      );
+      expect(logInfo).toHaveBeenCalledWith(logMessage);
     });
 
     it('should log the method, url and partial api key when a request is unsuccessful', async () => {
+      const logMessage = `Unsuccessful Request: GET /messages/${conversationId}/${messageId}, API Key: ******key`;
       await request(app)
         .get(`/messages/${conversationId}/${messageId}`)
         .set('Authorization', 'incorrect-key');
 
-      expect(logWarning).toHaveBeenCalledWith(
-        `Unsuccessful Request: GET /messages/${conversationId}/${messageId}, API Key: ******key`
-      );
+      expect(logWarning).toHaveBeenCalledWith(logMessage);
     });
   });
 });
