6db4f920ac6f72bf6838cde846617e0882d0dbfc
Jack Slater <jslater@thoughtworks.com>
2021-07-15 11:37:30 +0100

    [PRMT-1900] set up server side encryption for s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,8 +7,37 @@ resource "aws_s3_bucket" "ehr-repo-bucket" {
     enabled = false
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+
   tags = {
     CreatedBy   = var.repo_name
     Environment = var.environment
   }
 }
+
+resource "aws_s3_bucket_policy" "ehr-repo-bucket_policy" {
+
+  bucket = aws_s3_bucket.ehr-repo-bucket.id
+  policy = jsonencode({
+    "Statement": [
+      {
+        Effect: "Deny",
+        Principal: "*",
+        Action: "s3:*",
+        Resource: "${aws_s3_bucket.ehr-repo-bucket.arn}/*",
+        Condition: {
+          Bool: {
+            "aws:SecureTransport": "false"
+          }
+        }
+      }
+    ]
+  })
+}
+
