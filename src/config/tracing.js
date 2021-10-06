6134c3335abb70ffd492a9d4e3b9310d8b76e558
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-10-06 13:39:32 +0100

    [GreenPipeline] Updated dependencies and fixed opentelemetry breaking changes

--- src/config/tracing.js
+++ src/config/tracing.js
@@ -1,12 +1,12 @@
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { context, trace, propagation } from '@opentelemetry/api';
-import { HttpTraceContextPropagator } from '@opentelemetry/core';
-import { NodeTracerProvider } from '@opentelemetry/node';
+import { W3CTraceContextPropagator } from '@opentelemetry/core';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 
 const tracerProvider = new NodeTracerProvider({});
 
-propagation.setGlobalPropagator(new HttpTraceContextPropagator());
+propagation.setGlobalPropagator(new W3CTraceContextPropagator());
 
 tracerProvider.register();
 registerInstrumentations({
