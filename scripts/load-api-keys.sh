da666cceae9abf592ae2e9cc178b39d0b54947a8
Dan Moore <dan.moore1@nhs.net>
2022-03-22 12:28:33 +0000

    [PRMT-2599] Standardise load api keys using jmespath

--- scripts/load-api-keys.sh
+++ scripts/load-api-keys.sh
@@ -2,9 +2,13 @@
 AWS_DEFAULT_REGION=eu-west-2
 NHS_SERVICE=ehr-repo
 
+echo fetching api keys
+
 # Iterates through all api keys in ssm for producer
-for key in $(aws ssm get-parameters-by-path --region ${AWS_DEFAULT_REGION} --path "/repo/${NHS_ENVIRONMENT}/user-input/api-keys/ehr-repo/" --recursive --query 'Parameters[].Name' --output text)
+for key in $(aws ssm get-parameters-by-path --region ${AWS_DEFAULT_REGION} --path "/repo/${NHS_ENVIRONMENT}/user-input/api-keys/${NHS_SERVICE}/" --recursive --query 'Parameters[].Name' --output text)
 do
+  echo fetching api key
+
   # Gets the value of each api key
   value=$(aws ssm get-parameter --region ${AWS_DEFAULT_REGION} --with-decryption --name "${key}" --query Parameter.Value --output text)
   # Splits ssm path to get consumer name
