1e4e71fbb5bf9a9e98777cde6275931d44014562
Mario Giampietri <mario.giampietri1@nhs.net>
2022-05-24 15:34:30 +0100

    [PRMT-2640] revoked explicitly UPDATE and DELETE from application_role in messages and health_records tables

--- scripts/grant-db-permissions.sql
+++ scripts/grant-db-permissions.sql
@@ -3,5 +3,9 @@ GRANT SELECT, INSERT ON "SequelizeMeta" TO migration_role;
 
 -- Needs to execute after database was migrated
 GRANT SELECT, INSERT, UPDATE, DELETE ON health_checks TO application_role;
+
 GRANT SELECT, INSERT ON health_records TO application_role;
+REVOKE UPDATE, DELETE ON health_records FROM application_role;
+
 GRANT SELECT, INSERT ON messages TO application_role;
+REVOKE UPDATE, DELETE ON  messages FROM application_role;
