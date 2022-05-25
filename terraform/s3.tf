b25872637983c452a5f22ad5466421ce241c6f5c
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-05-25 12:00:12 +0100

    [PRMT-2640] Added s3 object lock for ehr-repo-bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,10 +3,6 @@ resource "aws_s3_bucket" "ehr-repo-bucket" {
   acl           = "private"
   force_destroy = true
 
-  versioning {
-    enabled = false
-  }
-
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -23,6 +19,25 @@ resource "aws_s3_bucket" "ehr-repo-bucket" {
   tags = {
     CreatedBy   = var.repo_name
     Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "ehr_repo_bucket_versioning" {
+  bucket = aws_s3_bucket.ehr-repo-bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "ehr_repo_object_lock" {
+  bucket = aws_s3_bucket.ehr-repo-bucket.bucket
+
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      years = 5000 # Never expires
+    }
   }
 }
 
