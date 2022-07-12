7f01e9fabccd0b6bbc43b5dabff1290f28a2c338
Mario Giampietri <mario.giampietri1@nhs.net>
2022-07-12 09:10:08 +0100

    [PRMT-2662] added a unit tests for sensible defaults on failure, auth, validation

--- src/api/patients/__tests__/delete-ehr-controller.test.js
+++ src/api/patients/__tests__/delete-ehr-controller.test.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 import app from '../../../app';
 import { deleteHealthRecordForPatient } from '../../../services/database/health-record-repository';
 import { initializeConfig } from '../../../config';
+import { logError } from '../../../middleware/logging';
 
 jest.mock('../../../services/database/health-record-repository');
 jest.mock('../../../middleware/logging');
@@ -16,11 +17,10 @@ describe('deleteEhrController', () => {
   });
 
   const authorizationKeys = 'correct-key';
+  const nhsNumber = '1234567890';
 
   describe('success', () => {
     it('should return 200 when controller invoked correctly', async () => {
-      const nhsNumber = '1234567890';
-
       deleteHealthRecordForPatient.mockResolvedValue('TBC');
 
       const res = await request(app)
@@ -32,71 +32,57 @@ describe('deleteEhrController', () => {
     });
   });
 
-  // describe('failure', () => {
-  //   const nhsNumber = '1234567890';
-  //
-  //   it('should return a 404 when no complete health record is found', async () => {
-  //     getCurrentHealthRecordIdForPatient.mockReturnValue(undefined);
-  //     const res = await request(app)
-  //       .get(`/patients/${nhsNumber}`)
-  //       .set('Authorization', authorizationKeys);
-  //
-  //     expect(res.status).toEqual(404);
-  //     expect(logInfo).toHaveBeenCalledWith('Did not find a complete patient health record');
-  //   });
-  //
-  //   it('should return a 503 when cannot get patient health record from database', async () => {
-  //     getCurrentHealthRecordIdForPatient.mockRejectedValue({});
-  //     const res = await request(app)
-  //       .get(`/patients/${nhsNumber}`)
-  //       .set('Authorization', authorizationKeys);
-  //
-  //     expect(res.status).toEqual(503);
-  //     expect(logError).toHaveBeenCalledWith('Could not retrieve patient health record', {});
-  //   });
-  // });
+  describe('failure', () => {
+    it('should return a 503 when an unexpected server error occurs', async () => {
+      deleteHealthRecordForPatient.mockRejectedValue({});
+      const res = await request(app)
+        .delete(`/patients/${nhsNumber}`)
+        .set('Authorization', authorizationKeys);
 
-  // describe('authentication', () => {
-  //   it('should return 401 when authentication keys are missing', async () => {
-  //     const nhsNumber = '1234567890';
-  //     const res = await request(app).get(`/patients/${nhsNumber}`);
-  //
-  //     expect(res.status).toBe(401);
-  //   });
-  //
-  //   it('should return 403 when authentication keys are incorrect', async () => {
-  //     const nhsNumber = '1234567890';
-  //     const res = await request(app)
-  //       .get(`/patients/${nhsNumber}`)
-  //       .set('Authorization', 'incorrect');
-  //
-  //     expect(res.status).toBe(403);
-  //   });
-  // });
+      expect(res.status).toEqual(503);
+      expect(logError).toHaveBeenCalledWith('Could not delete EHR record', {});
+    });
+  });
 
-  // describe('validation', () => {
-  //   it('should return 422 and an error message when nhsNumber is not numeric', async () => {
-  //     const nhsNumber = 'not-valid';
-  //     const errorMessage = { nhsNumber: "'nhsNumber' provided is not numeric" };
-  //
-  //     const res = await request(app)
-  //       .get(`/patients/${nhsNumber}`)
-  //       .set('Authorization', authorizationKeys);
-  //
-  //     expect(res.status).toBe(422);
-  //     expect(res.body.errors).toContainEqual(errorMessage);
-  //   });
-  //
-  //   it('should return 422 and an error message when nhsNumber is not 10 characters', async () => {
-  //     const nhsNumber = '123';
-  //     const errorMessage = { nhsNumber: "'nhsNumber' provided is not 10 characters" };
-  //
-  //     const res = await request(app)
-  //       .get(`/patients/${nhsNumber}`)
-  //       .set('Authorization', authorizationKeys);
-  //
-  //     expect(res.status).toBe(422);
-  //     expect(res.body.errors).toContainEqual(errorMessage);
-  //   });
-  // });
+  describe('authentication', () => {
+    it('should return 401 when authentication keys are missing', async () => {
+      const res = await request(app).delete(`/patients/${nhsNumber}`);
+
+      expect(res.status).toBe(401);
+    });
+
+    it('should return 403 when authentication keys are incorrect', async () => {
+      const res = await request(app)
+        .delete(`/patients/${nhsNumber}`)
+        .set('Authorization', 'incorrect');
+
+      expect(res.status).toBe(403);
+    });
+  });
+
+  describe('validation', () => {
+    it('should return 422 and an error message when nhsNumber is not numeric', async () => {
+      const invalidNhsNumber = 'not-valid';
+      const errorMessage = { nhsNumber: "'nhsNumber' provided is not numeric" };
+
+      const res = await request(app)
+        .get(`/patients/${invalidNhsNumber}`)
+        .set('Authorization', authorizationKeys);
+
+      expect(res.status).toBe(422);
+      expect(res.body.errors).toContainEqual(errorMessage);
+    });
+
+    it('should return 422 and an error message when nhsNumber is not 10 characters', async () => {
+      const invalidNhsNumber = '123';
+      const errorMessage = { nhsNumber: "'nhsNumber' provided is not 10 characters" };
+
+      const res = await request(app)
+        .get(`/patients/${invalidNhsNumber}`)
+        .set('Authorization', authorizationKeys);
+
+      expect(res.status).toBe(422);
+      expect(res.body.errors).toContainEqual(errorMessage);
+    });
+  });
 });
