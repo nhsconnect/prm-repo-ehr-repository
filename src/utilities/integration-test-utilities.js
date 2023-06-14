8a4868e102f9df2aa3dd019df42c256b5b9cc348
martin-nhs <martin.taylor22@nhs.net>
2023-06-14 16:59:25 +0100

    [PRMT-3415] Added functionality to integration-test-utilities.js

--- src/utilities/integration-test-utilities.js
+++ src/utilities/integration-test-utilities.js
@@ -1,6 +1,17 @@
-export const generateRandomNhsNumber = () => parseInt(Math.random().toString().slice(2, 12));
+import { v4 as uuidv4 } from 'uuid';
+
+export const generateRandomNhsNumber = () =>
+  Math.random().toString().slice(2, 12).replace('0', '5');
 
 export const generateMultipleRandomNhsNumbers = (amount) =>
   Array(amount)
     .fill(undefined)
-    .map(() => parseInt(Math.random().toString().slice(2, 12)));
+    .map(() => Math.random().toString().slice(2, 12).replace('0', '5'));
+
+export const generateRandomUUID = (isUppercase) =>
+  isUppercase ? uuidv4().toUpperCase() : uuidv4();
+
+export const generateMultipleUUID = (amount, isUppercase) =>
+  Array(amount)
+    .fill(undefined)
+    .map(() => (isUppercase ? uuidv4().toUpperCase() : uuidv4()));
