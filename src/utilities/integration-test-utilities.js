c3c6e6558ebd08b50f816b33aafe1130d1b09147
martin-nhs <martin.taylor22@nhs.net>
2023-06-20 16:28:23 +0100

    [PRMT-3415] Refactored integration-test-utilities.js, added functionality.

--- src/utilities/integration-test-utilities.js
+++ src/utilities/integration-test-utilities.js
@@ -1,4 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
+import ModelFactory from '../models';
+import { MessageType, modelName as messageModelName } from '../models/message';
+import { logError } from '../middleware/logging';
+import { getNow } from '../services/time';
+import { Op } from 'sequelize';
+
+const Message = ModelFactory.getByName(messageModelName);
 
 export const generateRandomNhsNumber = () =>
   Math.random().toString().slice(2, 12).replace('0', '5');
@@ -15,3 +22,33 @@ export const generateMultipleUUID = (amount, isUppercase) =>
   Array(amount)
     .fill(undefined)
     .map(() => (isUppercase ? uuidv4().toUpperCase() : uuidv4()));
+
+export const updateAllFragmentMessagesReceivedAtDateTime = async (messageIds) => {
+  const sequelize = ModelFactory.sequelize;
+  const transaction = await sequelize.transaction();
+
+  try {
+    await Message.update(
+      {
+        receivedAt: getNow(),
+      },
+      {
+        where: {
+          messageId: {
+            [Op.in]: messageIds,
+          },
+          type: {
+            [Op.eq]: MessageType.FRAGMENT,
+          },
+        },
+        transaction,
+      }
+    );
+
+    transaction.commit();
+  } catch (error) {
+    logError(error);
+    transaction.rollback();
+    throw error;
+  }
+};
