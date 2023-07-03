f4e57decb8319cd654421ed65bafaeef59246273
martin-nhs <martin.taylor22@nhs.net>
2023-07-03 12:36:05 +0100

    [PRMT-3415] Added further logic to verify a deletion's integrity.

--- src/services/database/delete-health-record.js
+++ src/services/database/delete-health-record.js
@@ -1,9 +1,10 @@
-import ModelFactory from '../../models';
 import { modelName as healthRecordModelName } from '../../models/health-record';
+import { NoHealthRecordFoundError, NoMessageFoundError } from '../../errors/errors';
 import { modelName as messageModelName } from '../../models/message';
-import { Op } from 'sequelize';
 import { logError } from '../../middleware/logging';
+import ModelFactory from '../../models';
 import { S3Service } from '../storage';
+import { Op } from 'sequelize';
 
 const HealthRecord = ModelFactory.getByName(healthRecordModelName);
 const Message = ModelFactory.getByName(messageModelName);
@@ -14,7 +15,7 @@ export const deleteHealthRecordAndMessages = async (conversationId) => {
   const s3 = new S3Service(`/${conversationId}/`);
 
   try {
-    await Message.destroy(
+    const messageResult = await Message.destroy(
       {
         where: {
           conversationId: {
@@ -26,7 +27,7 @@ export const deleteHealthRecordAndMessages = async (conversationId) => {
       { transaction }
     );
 
-    await HealthRecord.destroy(
+    const healthRecordResult = await HealthRecord.destroy(
       {
         where: {
           conversationId: {
@@ -37,6 +38,8 @@ export const deleteHealthRecordAndMessages = async (conversationId) => {
       },
       { transaction }
     );
+
+    validateDeletionIntegrity(messageResult, healthRecordResult, conversationId);
 
     await s3.deleteObject();
     await transaction.commit();
@@ -44,5 +47,13 @@ export const deleteHealthRecordAndMessages = async (conversationId) => {
     logError(error);
     transaction.rollback();
     throw error;
+  }
+};
+
+const validateDeletionIntegrity = (messageResult, healthRecordResult, conversationId) => {
+  if (messageResult === 0) {
+    throw new NoMessageFoundError(`with Conversation ID ${conversationId}.`);
+  } else if (healthRecordResult === 0) {
+    throw new NoHealthRecordFoundError(`with Conversation ID ${conversationId}.`);
   }
 };
