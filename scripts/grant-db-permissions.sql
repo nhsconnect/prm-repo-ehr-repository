1194785dac38adef07a19a07285e3af8190b5f67
martin-nhs <martin.taylor22@nhs.net>
2023-07-10 10:18:57 +0100

    [PRMT-3415] Addressed bugs in CloudWatch

--- scripts/grant-db-permissions.sql
+++ scripts/grant-db-permissions.sql
@@ -3,5 +3,5 @@ GRANT SELECT, INSERT ON "SequelizeMeta" TO migration_role;
 
 -- Needs to execute after database was migrated
 GRANT SELECT, INSERT, UPDATE, DELETE ON health_checks TO application_role;
-GRANT SELECT, INSERT, UPDATE, DELETE, ON health_records TO application_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON health_records TO application_role;
 GRANT SELECT, INSERT, UPDATE, DELETE ON messages TO application_role;
