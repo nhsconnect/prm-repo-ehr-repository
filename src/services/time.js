2061fe72ad3c0b58c33bfcb8a035fc40ed3a7982
martin-nhs <martin.taylor22@nhs.net>
2024-04-08 15:09:53 +0100

    [PRMT-4729] Changed from 12h clock to 24h clock.

--- src/services/time.js
+++ src/services/time.js
@@ -3,7 +3,7 @@ import moment from 'moment-timezone';
 export const getNow = () => new Date();
 
 export const getUKTimestamp = () => {
-  return moment().tz('Europe/London').format('YYYY-MM-DDThh:mm:ssZ');
+  return moment().tz('Europe/London').format('YYYY-MM-DDTHH:mm:ssZ');
 };
 export const TIMESTAMP_REGEX = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:00/;
 
