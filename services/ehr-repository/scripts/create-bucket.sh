62a58dabb5e3b2e760b054cb6434718816275783
Steph Torres <stephane.torres1@nhs.net>
2025-09-08 16:56:50 +0100

    [PRM-495] add conditional pathing to test-ecr action

--- services/ehr-repository/scripts/create-bucket.sh
+++ services/ehr-repository/scripts/create-bucket.sh
@@ -2,3 +2,5 @@
 set -e
 awslocal --endpoint-url=${LOCALSTACK_URL} s3 mb s3://${S3_BUCKET_NAME}
 set +e
+
+# TODO think this can go
--- services/ehr-repository/scripts/create-dynamodb-table.sh
+++ services/ehr-repository/scripts/create-dynamodb-table.sh
@@ -4,3 +4,5 @@ echo Creating a table for test in dynamodb-local...
 cd "$(dirname "$0")"
 aws --region ${{ var.AWS_REGION }} --endpoint=http://dynamodb-local:8000 dynamodb create-table --cli-input-json file://local-test-db-scheme.json --no-cli-page
 set +e
+
+#TODO think this can go
