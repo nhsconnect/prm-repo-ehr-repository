eb2aab4aa9075f283116ed6ab1f1ff37bc386657
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-09-16 10:42:34 +0100

    [PRM-495] continue removing dojo and gocd

--- services/ehr-repository/scripts/create-bucket.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-awslocal --endpoint-url=${LOCALSTACK_URL} s3 mb s3://${S3_BUCKET_NAME}
-set +e
-
-# TODO think this can go
--- services/ehr-repository/scripts/create-dynamodb-table.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo Creating a table for test in dynamodb-local...
-cd "$(dirname "$0")"
-aws --region ${{ var.AWS_REGION }} --endpoint=http://dynamodb-local:8000 dynamodb create-table --cli-input-json file://local-test-db-scheme.json --no-cli-page
-set +e
-
-#TODO think this can go
